5cdc2fbb24d442cc3b501ac166e55a9ebade9ef7
Angie <ajbhuige@outlook.com>
2019-11-26 09:09:56 +0100

    Comm

--- build/app.js
+++ build/app.js
@@ -21,12 +21,15 @@ class Game {
                 score: 200
             }
         ];
+        this.titleScreen();
+    }
+    startScreen() {
         this.writeAsteroidHeading();
         this.writeIntroText();
-        const asteroid_image = "./assets/images/SpaceShooterRedux/PNG/Meteors/meteorBrown_big1.png";
-        this.loadImage(asteroid_image, this.drawAsteroid);
-        const buttonImage = './assets/images/SpaceShooterRedux/PNG/UI/buttonBlue.png';
+        const buttonImage = "./assets/images/SpaceShooterRedux/PNG/UI/buttonBlue.png";
         this.loadImage(buttonImage, this.writeStartButton);
+        const asteroidImage = "./assets/images/SpaceShooterRedux/PNG/Meteors/meteorBrown_big1.png";
+        this.loadImage(asteroidImage, this.drawAsteroid);
     }
     writeAsteroidHeading() {
         this.ctx.font = "120px Minecraft";
@@ -38,28 +41,128 @@ class Game {
     writeIntroText() {
         this.ctx.font = "40px Minecraft";
         this.ctx.fillStyle = "white";
-        let text = this.ctx.measureText("Press play to start");
-        this.ctx.fillText("Press play to start", this.canvas.width / 2 -
+        let text = this.ctx.measureText("PRESS PLAY TO START");
+        this.ctx.fillText("PRESS PLAY TO START", this.canvas.width / 2 -
             text.width / 2, 400);
     }
     writeStartButton(img) {
         let x = this.canvas.width / 2 - img.width / 2;
         let y = 700;
         this.ctx.drawImage(img, x, y);
-        this.ctx.fillStyle = 'black';
         this.ctx.font = '20px Minecraft';
-        const text = this.ctx.measureText('play');
+        this.ctx.fillStyle = 'black';
+        const text = this.ctx.measureText('PLAY');
         x += img.width / 2 - text.width / 2;
         y += img.height / 3 * 2;
-        this.ctx.fillText('play', x, y);
+        this.ctx.fillText("PLAY", x, y);
     }
     drawAsteroid(img) {
         let image = this.ctx.measureText("img");
-        this.ctx.drawImage(img, this.canvas.width / 2 - image.width / 2, 500);
+        this.ctx.drawImage(img, this.canvas.width / 2 - img.width / 2, 500);
     }
     levelScreen() {
+        this.drawPlayerLives();
+        this.drawCurrentScore();
+        this.drawRandomAsteroids();
+        this.drawPlayerSpaceship();
+    }
+    drawPlayerLives() {
+        const livesImage = "./assets/images/SpaceShooterRedux/PNG/UI/playerLife3_blue.png";
+        this.loadImage(livesImage, this.writeLifeImage1ToLevelScreen);
+        this.loadImage(livesImage, this.writeLifeImage2ToLevelScreen);
+        this.loadImage(livesImage, this.writeLifeImage3ToLevelScreen);
+    }
+    writeLifeImage1ToLevelScreen(img) {
+        const x = 50;
+        const y = 50;
+        this.ctx.drawImage(img, x, y);
+    }
+    writeLifeImage2ToLevelScreen(img) {
+        const x = 90;
+        const y = 50;
+        this.ctx.drawImage(img, x, y);
+    }
+    writeLifeImage3ToLevelScreen(img) {
+        const x = 130;
+        const y = 50;
+        this.ctx.drawImage(img, x, y);
+    }
+    drawCurrentScore() {
+        const x = this.canvas.width - 200;
+        const y = 50;
+        this.ctx.font = '20px Minecraft';
+        this.ctx.fillStyle = 'white';
+        this.ctx.fillText(`SCORE = ${this.score}`, x, y);
+    }
+    drawRandomAsteroids() {
+        const asteroids = [
+            "./assets/images/SpaceShooterRedux/PNG/Meteors/meteorBrown_big1.png",
+            "./assets/images/SpaceShooterRedux/PNG/Meteors/meteorBrown_big2.png",
+            "./assets/images/SpaceShooterRedux/PNG/Meteors/meteorBrown_big3.png",
+            "./assets/images/SpaceShooterRedux/PNG/Meteors/meteorBrown_big4.png",
+            "./assets/images/SpaceShooterRedux/PNG/Meteors/meteorBrown_med1.png",
+            "./assets/images/SpaceShooterRedux/PNG/Meteors/meteorBrown_med3.png",
+            "./assets/images/SpaceShooterRedux/PNG/Meteors/meteorBrown_small1.png",
+            "./assets/images/SpaceShooterRedux/PNG/Meteors/meteorBrown_small2.png",
+            "./assets/images/SpaceShooterRedux/PNG/Meteors/meteorBrown_tiny1.png",
+            "./assets/images/SpaceShooterRedux/PNG/Meteors/meteorBrown_tiny2.png",
+            "./assets/images/SpaceShooterRedux/PNG/Meteors/meteorGrey_big1.png",
+            "./assets/images/SpaceShooterRedux/PNG/Meteors/meteorGrey_big2.png",
+            "./assets/images/SpaceShooterRedux/PNG/Meteors/meteorGrey_big3.png",
+            "./assets/images/SpaceShooterRedux/PNG/Meteors/meteorGrey_big4.png",
+            "./assets/images/SpaceShooterRedux/PNG/Meteors/meteorGrey_med1.png",
+            "./assets/images/SpaceShooterRedux/PNG/Meteors/meteorGrey_med2.png",
+            "./assets/images/SpaceShooterRedux/PNG/Meteors/meteorGrey_small1.png",
+            "./assets/images/SpaceShooterRedux/PNG/Meteors/meteorGrey_small2.png",
+            "./assets/images/SpaceShooterRedux/PNG/Meteors/meteorGrey_tiny1.png",
+            "./assets/images/SpaceShooterRedux/PNG/Meteors/meteorGrey_tiny2.png",
+        ];
+        const maxAsteroidsOnScreen = 10;
+        for (let i = 0; i < maxAsteroidsOnScreen; i++) {
+            const index = asteroids[this.randomNumber(0, asteroids.length)];
+            this.loadImage(index, this.actuallyDrawRandomAsteroids);
+        }
+    }
+    actuallyDrawRandomAsteroids(img) {
+        const x = this.randomNumber(0, this.canvas.width - img.width);
+        const y = this.randomNumber(0, this.canvas.height - img.height);
+        this.ctx.drawImage(img, x, y);
+    }
+    drawPlayerSpaceship() {
+        const spaceshipImage = "./assets/images/SpaceShooterRedux/PNG/playerShip1_blue.png";
+        this.loadImage(spaceshipImage, this.actuallyDrawPlayerSpaceship);
+    }
+    actuallyDrawPlayerSpaceship(img) {
+        const x = this.canvas.width / 2 - img.width / 2;
+        const y = this.canvas.height / 2 - img.height / 2;
+        this.ctx.drawImage(img, x, y);
     }
     titleScreen() {
+        this.drawEndScore();
+        this.drawHighscoresTitle();
+        this.drawHighScores();
+    }
+    drawEndScore() {
+        const x = this.canvas.width / 2 - 270;
+        const y = 300;
+        this.ctx.font = "60px Minecraft";
+        this.ctx.fillStyle = "white";
+        this.ctx.fillText(`FINAL SCORE = ${this.score}`, x, y);
+    }
+    drawHighscoresTitle() {
+        const text = this.ctx.measureText("HIGHSCORES");
+        const x = this.canvas.width / 2 - 90;
+        const y = 400;
+        this.ctx.font = "30px Minecraft";
+        this.ctx.fillStyle = "white";
+        this.ctx.fillText("HIGHSCORES", x, y);
+    }
+    drawHighScores() {
+        this.ctx.font = "20px Minecraft";
+        this.ctx.fillStyle = "white";
+        this.ctx.fillText("1: Loek - 40000 ", 700, 500);
+        this.ctx.fillText("2: Daan - 34000 ", 700, 550);
+        this.ctx.fillText("3: Rimmert - 200 ", 700, 600);
     }
     loadImage(source, callback) {
         let imageElement = new Image();
--- src/Game.ts
+++ src/Game.ts
@@ -38,17 +38,9 @@ class Game {
         ]
 
         // All screens: uncomment to activate
-        this.writeAsteroidHeading();
-        this.writeIntroText();
-
-        const asteroid_image = "./assets/images/SpaceShooterRedux/PNG/Meteors/meteorBrown_big1.png"
-        this.loadImage(asteroid_image, this.drawAsteroid);
-
-        const buttonImage: string = './assets/images/SpaceShooterRedux/PNG/UI/buttonBlue.png';
-        this.loadImage(buttonImage, this.writeStartButton);
-
+        // this.startScreen();
         // this.levelScreen();
-        // this.titleScreen();
+        this.titleScreen();
 
     }
 
@@ -56,61 +48,66 @@ class Game {
     /**
      * Method to initialize the splash screen
      */
-    public writeAsteroidHeading() {
-        //1. add 'Asteroids' text
-        this.ctx.font = "120px Minecraft";              // size and font        
-        this.ctx.fillStyle = "white";                   // colour of text, otherwise it won't show up
-        let text = this.ctx.measureText("Asteroids");   // to center the text
-        this.ctx.fillText(
-        "Asteroids",
-        this.canvas.width/2 -
-        text.width/2, 150);
-
-        }
-
-    //2. add 'Press to play' text
-    public writeIntroText() {
-        this.ctx.font = "40px Minecraft";
-        this.ctx.fillStyle = "white";
-        let text = this.ctx.measureText("Press play to start");
-        this.ctx.fillText(
-            "Press play to start",
-            this.canvas.width/2 -
-            text.width/2, 400
-        );
-
-    }
-
-    public writeStartButton(img: HTMLImageElement){
-            let x: number = this.canvas.width/2 - img.width/2;
-            let y: number = 700;
-    
-            this.ctx.drawImage(img, x, y);
-    
-            this.ctx.fillStyle = 'black';
-            this.ctx.font = '20px Minecraft';
-            const text = this.ctx.measureText('play');
-    
-            x += img.width/2 - text.width/2;
-            y += img.height/3 * 2;
-    
-            this.ctx.fillText('play', x, y);
+    public startScreen() {
+        // 1. Add "Asteroids" text
+        this.writeTextToCanvas("Asteroids", 120, this.canvas.width/2, 150)
         
+        this.writeIntroText();
+
+        const buttonImage = "./assets/images/SpaceShooterRedux/PNG/UI/buttonBlue.png";      // image for start button
+        this.loadImage(buttonImage, this.writeStartButton);                                 // loading image for start button
+
+        const asteroidImage = "./assets/images/SpaceShooterRedux/PNG/Meteors/meteorBrown_big1.png"  // image for asteroids
+        this.loadImage(asteroidImage, this.drawAsteroid);                                           // loading image for asteroid
     }
 
-// 3.Maak een method writeStartButton() die de button met de tekst
-// ‘START’ op de juiste plek op het canvas schrijft.
-// 4.Maak de method drawAsteroid() die de asteroid mbv het plaatje in
-// de assets map op de juiste plek op het canvas tekent.
+    // public startScreen() {
+    //     // 1. add 'Asteroids' text
+    //     this.writeTextToCanvas("Asteroids", 140, this.canvas.width / 2, 150);
 
-        //3. add button with 'start' text
+    //     // 2. add 'Press to play' text
+    //     this.writeTextToCanvas("PRESS PLAY TO START", 40, this.canvas.width / 2, this.canvas.height / 2 - 20);
 
+    //     // 3. add button with 'start' text
+    //     const asteroidFileName = "./assets/images/SpaceShooterRedux/PNG/UI/buttonBlue.png";
+    //     this.loadImage(asteroidFileName, this.writeStartButtonToStartScreen);
+    //     // 4. add Asteroid image
+    //     const startButtonFileName = "./assets/images/SpaceShooterRedux/PNG/Meteors/meteorBrown_big1.png";
+    //     this.loadImage(startButtonFileName, this.writeAsteroidImageToStartScreen);
+    // }
+
+    //2. Method to add 'Press to play' text
+    public writeIntroText() {
+        this.ctx.font = "40px Minecraft";                           // size and font
+        this.ctx.fillStyle = "white";                               // colour of text, otherwise it won't show up
+        let text = this.ctx.measureText("PRESS PLAY TO START");     // to center the tex
+        this.ctx.fillText(
+            "PRESS PLAY TO START",
+            this.canvas.width/2 -
+            text.width/2, 400);
+    }
+
+    // 3. Method to add start button
+    public writeStartButton(img: HTMLImageElement) {
+        let x: number = this.canvas.width/2 - img.width/2;          // define position to center image and text
+        let y: number = 700;
+
+        this.ctx.drawImage(img, x, y);                              // place the image
+
+        this.ctx.font = '20px Minecraft';                           // size and font
+        this.ctx.fillStyle = 'black';                               // colour of text, otherwise it won't show up
+        const text = this.ctx.measureText('PLAY');
+
+        x += img.width/2 - text.width/2;
+        y += img.height/3 * 2;
+
+        this.ctx.fillText("PLAY", x, y);  
+    }
 
     //4. add Asteroid image
     private drawAsteroid(img: HTMLImageElement) {
         let image = this.ctx.measureText("img");
-        this.ctx.drawImage(img, this.canvas.width/2 - image.width/2, 500);  // to center the image
-
+        this.ctx.drawImage(img, this.canvas.width/2 - img.width/2, 500);  // to center the image
     }
 
     
@@ -119,10 +116,106 @@ class Game {
      * Method to initialize the level screen
      */
     public levelScreen() {
-        //1. load life images
-        //2. draw current score
-        //3. draw random asteroids
-        //4. draw player spaceship
+    this.drawPlayerLives();
+    this.drawCurrentScore();
+    this.drawRandomAsteroids();
+    this.drawPlayerSpaceship();
+    }
+    
+    //1. Method to add lives
+    private drawPlayerLives() {
+        const livesImage = "./assets/images/SpaceShooterRedux/PNG/UI/playerLife3_blue.png"      // image for lives
+        this.loadImage(livesImage, this.writeLifeImage1ToLevelScreen);                          
+        this.loadImage(livesImage, this.writeLifeImage2ToLevelScreen);
+        this.loadImage(livesImage, this.writeLifeImage3ToLevelScreen);
+    }
+
+    // Methods to actually write life images to level screen
+    private writeLifeImage1ToLevelScreen(img: HTMLImageElement){
+        const x = 50;
+        const y = 50;
+        this.ctx.drawImage(img, x, y);
+        
+    }
+
+    private writeLifeImage2ToLevelScreen(img: HTMLImageElement){
+        const x = 90;
+        const y = 50;
+        this.ctx.drawImage(img, x, y);
+        
+    }
+
+    private writeLifeImage3ToLevelScreen(img: HTMLImageElement){
+        const x = 130;
+        const y = 50;
+        this.ctx.drawImage(img, x, y);
+        
+    }
+
+    //2. Method to draw current score
+    private drawCurrentScore() {
+        // placing of the text
+        const x = this.canvas.width - 200;
+        const y = 50;
+        
+        this.ctx.font = '20px Minecraft';                           // size and font
+        this.ctx.fillStyle = 'white';                               // colour of text, otherwise it won't show up
+        this.ctx.fillText(`SCORE = ${this.score}`, x, y);           // determines the actual text
+    }
+
+    //3. Method to draw random asteroids
+    private drawRandomAsteroids() {
+        // All asteroid images
+        const asteroids = 
+        [
+            "./assets/images/SpaceShooterRedux/PNG/Meteors/meteorBrown_big1.png",
+            "./assets/images/SpaceShooterRedux/PNG/Meteors/meteorBrown_big2.png",
+            "./assets/images/SpaceShooterRedux/PNG/Meteors/meteorBrown_big3.png",
+            "./assets/images/SpaceShooterRedux/PNG/Meteors/meteorBrown_big4.png",
+            "./assets/images/SpaceShooterRedux/PNG/Meteors/meteorBrown_med1.png",
+            "./assets/images/SpaceShooterRedux/PNG/Meteors/meteorBrown_med3.png",
+            "./assets/images/SpaceShooterRedux/PNG/Meteors/meteorBrown_small1.png",
+            "./assets/images/SpaceShooterRedux/PNG/Meteors/meteorBrown_small2.png",
+            "./assets/images/SpaceShooterRedux/PNG/Meteors/meteorBrown_tiny1.png",
+            "./assets/images/SpaceShooterRedux/PNG/Meteors/meteorBrown_tiny2.png",
+            "./assets/images/SpaceShooterRedux/PNG/Meteors/meteorGrey_big1.png",
+            "./assets/images/SpaceShooterRedux/PNG/Meteors/meteorGrey_big2.png",
+            "./assets/images/SpaceShooterRedux/PNG/Meteors/meteorGrey_big3.png",
+            "./assets/images/SpaceShooterRedux/PNG/Meteors/meteorGrey_big4.png",
+            "./assets/images/SpaceShooterRedux/PNG/Meteors/meteorGrey_med1.png",
+            "./assets/images/SpaceShooterRedux/PNG/Meteors/meteorGrey_med2.png",
+            "./assets/images/SpaceShooterRedux/PNG/Meteors/meteorGrey_small1.png",
+            "./assets/images/SpaceShooterRedux/PNG/Meteors/meteorGrey_small2.png",
+            "./assets/images/SpaceShooterRedux/PNG/Meteors/meteorGrey_tiny1.png",
+            "./assets/images/SpaceShooterRedux/PNG/Meteors/meteorGrey_tiny2.png",
+        ]
+
+        const maxAsteroidsOnScreen: number = 10;    // sets the maximum of asteroids on screen
+
+        // loop to randomly show asteroids
+        for (let i = 0; i < maxAsteroidsOnScreen; i++) {
+            const index: string = asteroids[this.randomNumber(0, asteroids.length)];
+            this.loadImage(index, this.actuallyDrawRandomAsteroids); 
+        }
+    }
+
+    // method to actually show the image of the asteroids
+    private actuallyDrawRandomAsteroids(img: HTMLImageElement){
+        const x = this.randomNumber(0, this.canvas.width - img.width);
+        const y = this.randomNumber(0, this.canvas.height - img.height);
+        this.ctx.drawImage(img, x, y);
+    }
+
+    //4. draw player spaceship
+    private drawPlayerSpaceship() {
+        const spaceshipImage = "./assets/images/SpaceShooterRedux/PNG/playerShip1_blue.png"
+        this.loadImage(spaceshipImage, this.actuallyDrawPlayerSpaceship);
+    }
+    
+    private actuallyDrawPlayerSpaceship(img: HTMLImageElement){
+        const x = this.canvas.width/2 - img.width/2;          // define position to center image and text
+        const y = this.canvas.height/2 - img.height/2;
+        this.ctx.drawImage(img, x, y);
     }
 
     //-------- Title screen methods -------------------------------------
@@ -131,12 +224,66 @@ class Game {
     * Method to initialize the title screen
     */
     public titleScreen() {
-        //1. draw your score
-        //2. draw all highscores
+        this.drawEndScore();
+        this.drawHighscoresTitle();
+        this.drawHighScores();
+        }
+
+    //1. draw your score
+    private drawEndScore() {
+        const x = this.canvas.width/2 - 270;
+        const y = 300;
+
+        this.ctx.font = "60px Minecraft";
+        this.ctx.fillStyle = "white";
+ 
+        this.ctx.fillText(`FINAL SCORE = ${this.score}`, x, y);           
     }
 
-    //-------Generic canvas methods ----------------------------------
+    private drawHighscoresTitle() {
+        const text = this.ctx.measureText("HIGHSCORES");
+        const x = this.canvas.width/2 - 90;
+        const y = 400;
 
+        this.ctx.font = "30px Minecraft";
+        this.ctx.fillStyle = "white";
+ 
+        this.ctx.fillText("HIGHSCORES", x, y);           
+    }
+
+    private drawHighScores() {
+        this.ctx.font = "20px Minecraft";
+        this.ctx.fillStyle = "white";
+
+        this.ctx.fillText("1: Loek - 40000 ", 700, 500);
+        this.ctx.fillText("2: Daan - 34000 ", 700, 550);           
+        this.ctx.fillText("3: Rimmert - 200 ", 700, 600);                      
+
+    }
+    
+    //-------Generic canvas methods ----------------------------------
+    /**
+     * Writes text to the canvas
+     * @param {string} text - Text to write
+     * @param {number} fontSize - Font size in pixels
+     * @param {number} xCoordinate - Horizontal coordinate in pixels
+     * @param {number} yCoordinate - Vertical coordinate in pixels
+     * @param {string} alignment - Where to align the text
+     * @param {string} colour - The color of the text
+     */
+    public writeTextToCanvas(
+        text: string,
+        fontSize: number = 20,
+        xCoordinate: number,
+        yCoordinate: number,
+        alignment: CanvasTextAlign = "center",
+        colour: string = "white",
+    ) {
+        this.ctx.font = `${fontSize}px Minecraft`;
+        this.ctx.fillStyle = colour;
+        this.ctx.textAlign = alignment;
+        this.ctx.fillText(text, xCoordinate, yCoordinate);
+    }
     /**
      * Loads an image file into the DOM and writes it to the canvas. After the
      * image is loaded and ready to be drawn to the canvas, the specified
